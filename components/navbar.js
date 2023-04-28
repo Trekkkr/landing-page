3f2027346fd01dbd94c9f5028873c2230ecb0d63
Oscar Estrada <arturostrada2020@gmail.com>
2023-04-28 17:03:16 -0600

    edit navbar

--- components/navbar.js
+++ components/navbar.js
@@ -20,24 +20,28 @@ const Navbar = () => {
           {({ open }) => (
             <>
               <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
-                <Link href="/">
-                  <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
-                    <span>
+                <Link className='logo-dark' href="/">
                       <Image
-                        src="/img/logo.svg"
-                        alt="N"
-                        width="32"
-                        height="32"
-                        className="w-8"
+                        src="/logo.png"
+                        alt="Trekkkr logo"
+                        width="120"
+                        height="60"
+                        className="w-24"
                       />
-                    </span>
-                    <span>Nextly</span>
-                  </span>
+                </Link>
+                <Link className='logo-light' href="/">
+                      <Image
+                        src="/logo-white.png"
+                        alt="Trekkkr logo"
+                        width="120"
+                        height="60"
+                        className="w-24"
+                      />
                 </Link>
 
                 <Disclosure.Button
                   aria-label="Toggle Menu"
-                  className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700">
+                  className="px-2 py-1 ml-auto text-gray-500 rounded-lg lg:hidden hover:text-[#EC5153] focus:text-[#EC5153] focus:bg-gray-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700">
                   <svg
                     className="w-6 h-6 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
@@ -61,12 +65,12 @@ const Navbar = () => {
                 <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
                   <>
                     {navigation.map((item, index) => (
-                      <Link key={index} href="/" className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none">
+                      <Link key={index} href={`#${item}`} className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-[#EC5153] focus:text-[#EC5153] focus:bg-gray-100 dark:focus:bg-gray-800 focus:outline-none">
                           {item}
                       </Link>
                     ))}
-                    <Link href="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">         
-                        Get Started
+                    <Link href="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-[#EC5153] rounded-md lg:ml-5">         
+                        Waitlist
                     </Link>
                   </>
                 </Disclosure.Panel>
@@ -80,7 +84,7 @@ const Navbar = () => {
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
             {navigation.map((menu, index) => (
               <li className="mr-3 nav__item" key={index}>
-                <Link href="/" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
+                <Link href={`#${menu}`} className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-[#EC5153] hover:bg-gray-50 focus:text-[#EC5153] focus:bg-gray-100 focus:outline-none dark:focus:bg-gray-800">
                     {menu}
                 </Link>
               </li>
@@ -89,8 +93,8 @@ const Navbar = () => {
         </div>
 
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
-          <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
-              Get Started
+          <Link href="/" className="px-6 py-2 text-white bg-[#EC5153] rounded-md md:ml-5">
+              Waitlist
           </Link>
 
           <ThemeChanger />
