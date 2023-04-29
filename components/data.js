d33ce282b81f403864ab06e93ccf25998ef34de2
Oscar Estrada <arturostrada2020@gmail.com>
2023-04-28 20:43:09 -0600

    edit data

--- components/data.js
+++ components/data.js
@@ -1,57 +1,69 @@
 import {
   FaceSmileIcon,
   ChartBarSquareIcon,
-  CursorArrowRaysIcon,
-  DevicePhoneMobileIcon,
+  CreditCardIcon,
   AdjustmentsHorizontalIcon,
-  SunIcon,
+  LockClosedIcon,
+  CurrencyDollarIcon,
+  ShieldCheckIcon,
+  UsersIcon
 } from "@heroicons/react/24/solid";
 
-import benefitOneImg from "../public/img/benefit-one.png";
-import benefitTwoImg from "../public/img/benefit-two.png";
+import benefitOneImg from "../public/images/benefit-one.png";
+import benefitTwoImg from "../public/images/benefit-two.png";
 
 const benefitOne = {
-  title: "Highlight your benefits",
-  desc: "You can use this space to highlight your first benefit or a feature of your product. It can also contain an image or Illustration like in the example along with some bullet points.",
+  title: "¿Cómo lo haremos?",
+  desc: "Estos son algunos de las soluciones que Trekkkr ofrecerá a los senderistas, guías independientes y agencias:",
   image: benefitOneImg,
   bullets: [
     {
-      title: "Understand your customers",
-      desc: "Then explain the first point breifly in one or two lines.",
+      title: "Conoce nuevos senderistas",
+      desc: "No importa donde te encuentres, en Trekkkr podras unirte a eventos públicos y privados con otros senderistas",
       icon: <FaceSmileIcon />,
     },
     {
-      title: "Improve acquisition",
-      desc: "Here you can add the next benefit point.",
-      icon: <ChartBarSquareIcon />,
+      title: "Controla quien accede a tus eventos",
+      desc: "Elige si tus eventos son públicos o exclusivamente para tus amigos dentro de la comunidad Trekkkr",
+      icon: <AdjustmentsHorizontalIcon />,
     },
     {
-      title: "Drive customer retention",
-      desc: "This will be your last bullet point in this section.",
-      icon: <CursorArrowRaysIcon />,
+      title: "Camina únicamente con personas de tu interés",
+      desc: "Controla si tus eventos son solo para mujeres, hombres, grupos mixtos y el nivel mínimo de experiencia y condición física para acceder a tus eventos",
+      icon: <UsersIcon />,
+    },
+    {
+      title: "Mide y mejora tu nivel de experiencia",
+      desc: "Evalua a tus compañeros y guías de viaje. Accede a recomendaciones de senderistas experimentados para mejorar tu nivel de experiencia",
+      icon: <ChartBarSquareIcon />,
     },
   ],
 };
 
 const benefitTwo = {
-  title: "Offer more benefits here",
-  desc: "You can use this same layout with a flip image to highlight your rest of the benefits of your product. It can also contain an image or Illustration as above section along with some bullet points.",
+  title: "Otros beneficios",
+  desc: "Monetiza y disfruta tu pasión sin complicaciones",
   image: benefitTwoImg,
   bullets: [
     {
-      title: "Mobile Responsive Template",
-      desc: "Nextly is designed as a mobile first responsive template.",
-      icon: <DevicePhoneMobileIcon />,
+      title: "Accede a eventos exclusivos de Trekkkr",
+      desc: "Recibe invitaciones a eventos exclusivos con senderistas profesionales de acuerdo a tu nivel de experiencia",
+      icon: <LockClosedIcon />,
     },
     {
-      title: "Powered by Next.js & TailwindCSS",
-      desc: "This template is powered by latest technologies and tools.",
-      icon: <AdjustmentsHorizontalIcon />,
+      title: "Monetiza tus viajes",
+      desc: "Tu decides si tus eventos son gratuitos o de pago. Acepta pagos en efectivo u online y transfiere las ganancias a tu cuenta bancaria fácilmente",
+      icon: <CurrencyDollarIcon />,
     },
     {
-      title: "Dark & Light Mode",
-      desc: "Nextly comes with a zero-config light & dark mode. ",
-      icon: <SunIcon />,
+      title: "Acepta anticipos",
+      desc: "Controla si tus eventos de pago requieren anticipos para reservar un lugar en tu evento",
+      icon: <CreditCardIcon />,
+    },
+    {
+      title: "Garantiza la seguridad en tus aventuras",
+      desc: "Reporta actos indebidos que pongan en riesgo tu integridad y la de tus compañeros en cada uno de tus eventos",
+      icon: <ShieldCheckIcon />,
     },
   ],
 };
