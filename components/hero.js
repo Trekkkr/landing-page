a9801e52f3ea134b79cda5fd7f1daa961646885e
Oscar Estrada <arturostrada2020@gmail.com>
2023-04-28 21:23:42 -0600

    edit hero

--- components/hero.js
+++ components/hero.js
@@ -12,10 +12,10 @@ const Hero = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Comparte aventuras con otros senderistas
+              Descubre nuevos caminos
             </h1>
             <p className="py-8 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              Crea, organiza y accede a eventos públicos o privados con otros senderistas
+              La plataforma para senderistas que te permite buscar, organizar y acceder a las mejores aventuras
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
