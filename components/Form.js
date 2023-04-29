7ad781032311cff5a761fa8cc2e6dd3798808fdc
Oscar Estrada <arturostrada2020@gmail.com>
2023-04-29 00:17:47 -0600

    create Form component

---
+++ components/Form.js
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import Container from './container'
+
+const inputs = [
+  { type: 'text', name: 'name', placeholder: 'Nombre', required: true },
+  { type: 'email', name: 'email', placeholder: 'Email', required: true },
+  { type: 'text', name: 'country', placeholder: 'País', required: true },
+  { type: 'text', name: 'message', placeholder: 'Mensaje', required: true }
+]
+
+export const Form = () => {
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    country: '',
+    message: ''
+  })
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    setValues((prevState) => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    console.log(values)
+
+    const url = '/api/submit'
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
+      })
+
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.error('API error:', error)
+    }
+  }
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit} className='w-full max-w-2xl p-2 mx-auto'>
+        {inputs.map(input => (
+          <input
+            key={input.name}
+            type={input.type}
+            placeholder={input.placeholder}
+            name={input.name}
+            value={values[input.name]}
+            onChange={handleInputChange}
+            required={input.required}
+            className='border border-gray-300 dark:border-trueGray-800 dark:bg-trueGray-800 w-full rounded-xl p-4 text-lg mb-5'
+          />
+        ))}
+
+        <input type='submit' value='Enviar' className='p-4 rounded-lg bg-blue-600 hover:bg-blue-700 cursor-pointer w-full text-white' />
+      </form>
+    </Container>
+  )
+}
