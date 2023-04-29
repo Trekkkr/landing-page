9e2d9cbc8e8fb776bdbb702ca61c82d1195fbdb5
Oscar Estrada <arturostrada2020@gmail.com>
2023-04-28 20:41:47 -0600

    edit cta component

--- components/cta.js
+++ components/cta.js
@@ -4,22 +4,22 @@ import Container from "./container";
 const Cta = () => {
   return (
     <Container>
-      <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
+      <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-[#ec5153] px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
         <div className="flex-grow text-center lg:text-left">
           <h2 className="text-2xl font-medium lg:text-3xl">
-            Ready to try-out this template?
+            ¿Listo para ser parte de Trekkkr?
           </h2>
           <p className="mt-2 font-medium text-white text-opacity-90 lg:text-xl">
-            Don&apos;t let your visitors see a poor landing.
+            Siguenos en instagram
           </p>
         </div>
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
           <a
-            href="https://github.com/web3templates"
+            href="/"
             target="_blank"
             rel="noopener"
-            className="inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-10 lg:py-5 ">
-            Download for Free
+            className="inline-block py-3 mx-auto text-lg font-medium text-center text-[#ec5153] bg-white rounded-md px-7 lg:px-10 lg:py-5 ">
+            Follow
           </a>
         </div>
       </div>
