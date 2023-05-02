b5c04718be2c58aac38263a3e33df66c5e50c5d9
Oscar Estrada <arturostrada2020@gmail.com>
2023-05-02 17:14:52 -0600

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { ThemeProvider } from 'next-themes'
 import '../css/tailwind.css'
 
 function MyApp ({ Component, pageProps }) {
+  const isProd = process.env.NODE_ENV === 'production'
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      if (isProd) {
+        window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+          page_path: url
+        })
+      }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.event.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ThemeProvider attribute='class'>
       <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,33 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import Link from 'next/link'
+// Importing the Google Analytics Measurement ID from the environment variable
+const gtag = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`
+const isProd = process.env.NODE_ENV === 'production'
 
 class MyDocument extends Document {
   render () {
     return (
       <Html>
         <Head>
+          {isProd && (
+            <>
+              {/* Google Analytics Measurement ID */}
+              <script async src={gtag} />
+              {/* Inject the GA tracking code with the measurement ID */}
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname
+                });
+              `
+                }}
+              />
+            </>
+          )}
           <Link
             href='https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap'
             rel='stylesheet'
