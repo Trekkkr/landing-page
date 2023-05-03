6528cfc8717b85187f85a0c9939fb2ff3f18d0f8
Oscar Estrada <arturostrada2020@gmail.com>
2023-05-03 09:12:42 -0600

    fix google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 import Link from 'next/link'
 
 class MyDocument extends Document {
@@ -6,10 +7,27 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+
+          <Script
+            src='https://www.googletagmanager.com/gtag/js?id=G-PJN6FDG54F'
+            strategy='afterInteractive'
+          />
+
+          <Script id='google-analytics' strategy='afterInteractive'>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-PJN6FDG54F');
+            `}
+          </Script>
+
           <Link
             href='https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap'
             rel='stylesheet'
           />
+
         </Head>
         <body>
           <Main />
